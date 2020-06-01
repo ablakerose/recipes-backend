7ec0637f5c105813ff33b0a58029fcdcf6bad62b
ablakerose <bdecktor@gmail.com>
2020-06-01 14:37:36 -0400

    update user and recipe resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :recipes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :recipes
-    end
+        end
   end
 end
--- db/migrate/20200601183535_create_recipes.rb
+++ db/migrate/20200601183535_create_recipes.rb
@@ -1,9 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.string :body
+      t.string :user_id
       t.string :title
-      
+      t.string :ingredients
+      t.string :instructions
+      t.string :image
+
       t.timestamps
     end
   end
