ecdf4c046d574c23f04a6f36078ee55c339b74a2
ablakerose <bdecktor@gmail.com>
2020-06-10 10:41:13 -0400

    add skeleton of method for formatting recipe font

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ApplicationRecord
     belongs_to :user
     validates :title, presence: true
+
+    def set_uniform_text(recipe)
+        #make the text the same font, size
+        #format the spacing
+    end
+
 end
