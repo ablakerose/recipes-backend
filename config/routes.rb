9eda2c3c95376685817d40bc5269cc3a8c3f1fbb
ablakerose <bdecktor@gmail.com>
2020-06-10 10:41:43 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :recipes
-        end
-      end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do  
+      resources :recipes       
       resources :users
-        end
+    end
   end
 end
+
